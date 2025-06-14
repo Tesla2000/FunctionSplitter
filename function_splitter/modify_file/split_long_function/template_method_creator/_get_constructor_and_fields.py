dc8a9f7fcc4f5b57dedfd6b3a27b4eb9044ce8ee
Tesla2000 <fratajczak124@gmail.com>
2025-06-14 22:26:26 +0200

    Added constructor info NamedTuple

--- function_splitter/modify_file/split_long_function/template_method_creator/_get_constructor_and_fields.py
+++ function_splitter/modify_file/split_long_function/template_method_creator/_get_constructor_and_fields.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import NamedTuple
+
 from libcst import FunctionDef
 from libcst import Module
 from modify_file.split_long_function.template_method_components import (
@@ -10,12 +12,18 @@ from ._constructor_fields_extractor import constructor_fields_extractor
 from ._variable_name_replacer import VariableNameReplacer
 
 
+class _ConstructorInfo(NamedTuple):
+    constructor: Constructor
+    self_names: set[str]
+    self_function_code: str
+
+
 def get_constructor_and_fields(
     function_node: FunctionDef,
-) -> tuple[Constructor, set[str], str]:
+) -> _ConstructorInfo:
     replacer = VariableNameReplacer(function_node)
     self_function_code = Module([function_node.visit(replacer)]).code
     self_names = replacer.replaced_names
     constructor_fields = constructor_fields_extractor(function_node)
     constructor = Constructor(fields=list(constructor_fields))
-    return constructor, self_names, self_function_code
+    return _ConstructorInfo(constructor, self_names, self_function_code)
